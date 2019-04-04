5ee5196e863cf9d7e19ba6a922af1d3fae7f8320
Mickilous <mickilous@gmail.com>
2019-04-04 16:01:57 +0200

    [REFACTORE]Light naming and comments

--- deserve/client.go
+++ deserve/client.go
@@ -38,14 +38,14 @@ func (c *Client) IsGreetable(userId string) bool {
 		return defaultRet
 	}
 
-	body, errRead := ioutil.ReadAll(response.Body)
-	if errRead != nil {
-		c.logger.Printf("The HTTP body read failed with error %v\n", errRead)
-		return defaultRet //In doubt let's greet and do not be impolite
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		c.logger.Printf("The HTTP body read failed with error %v\n", err)
+		return defaultRet
 	}
 	if response.StatusCode != http.StatusOK {
 		c.logger.Printf("The HTTP request failed with HTTP Code : %v - %v", response.StatusCode, string(body))
-		return defaultRet //In doubt let's greet and do not be impolite
+		return defaultRet
 	}
 
 	deserveMsg := &Message{}
